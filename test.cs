79ec1ae2c7c7a451fc9d13bd02b9a309031d2db3
Nasim Zarei <Nasim.Zarei@drheuer.de>
2022-06-01 13:51:03 +0200

    ein  klasse hinzufugen

---
+++ test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+		 class test
+		 {
+					public int id { get; set; }
+					public string name { get; set; }
+		 }
+}
